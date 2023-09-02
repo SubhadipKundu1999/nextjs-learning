e3e19d1e5fc38ae454f14a76308411539ee5b4a0
SubhadipKundu1999 <subhadipkundu1000@gmail.com>
2023-09-02 12:27:01 +0530

    static html

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-blue-500">About page</h1>
+    </div>
+  )
+}
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import User from "./components/user.js"
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Contact page</h1>
+      <User/>
+    </div>
+  )
+}
